600d9455bde085b86fb619c8f8d8b89c6d65c139
Fardin Fahim <fardin.fahim@yahoo.com>
2022-06-12 00:27:57 +0100

    call MusicController methods

--- Assets/Scripts/NoiseIntensifier.cs
+++ Assets/Scripts/NoiseIntensifier.cs
@@ -11,6 +11,7 @@ public class NoiseIntensifier : MonoBehaviour
     [SerializeField] private float effectRadius;
     [SerializeField] private Transform player;
     [SerializeField] private MushroomAreas mushroomArea;
+    [SerializeField] private MusicController musicController;
 
     // Start is called before the first frame update
     void Start()
@@ -26,19 +27,27 @@ public class NoiseIntensifier : MonoBehaviour
         float distance = Vector3.Distance(transform.position,
             player.position);
         
-        // either nothing happens or a lot happens
-        if (distance <= nearRadius)
+        
+        if (distance <= nearRadius) // play the peaceful music and remove shaders
         {
+            musicController.OnEnterMushroomNear(mushroomArea);
+
+
             effectStackController.MushroomArea = MushroomAreas.None;
+            
         }
-        else if (distance <= effectRadius)
+        else if (distance <= effectRadius) // play the high intensity music and shaders increasing in intensity as you get closer 
         {
+            musicController.OnExitSpawn();
+            musicController.OnEnterMushroomArea(mushroomArea);
+
             float effectPercentage = 1 - (distance - nearRadius) / (effectRadius - nearRadius);
             effectStackController.MushroomArea = mushroomArea;
             effectStackController.Intensity = effectPercentage;
         }
         else
         {
+            musicController.OnExitMushroom();
             effectStackController.MushroomArea = MushroomAreas.None;
         }
     }
