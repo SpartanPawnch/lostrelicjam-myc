6ec64297bb40b5674138210c5113617b447dd367
alexge50 <alexge50.x@gmail.com>
2022-06-12 03:54:58 +0100

    multiple shrooms

--- Assets/Scripts/NoiseIntensifier.cs
+++ Assets/Scripts/NoiseIntensifier.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class NoiseIntensifier : MonoBehaviour
@@ -9,35 +10,52 @@ public class NoiseIntensifier : MonoBehaviour
 
     [SerializeField] private float nearRadius;
     [SerializeField] private float effectRadius;
-    [SerializeField] private Transform player;
-    [SerializeField] private MushroomAreas mushroomArea;
     [SerializeField] private MusicController musicController;
+    
+    private GameObject[] shrooms;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-        //player = GameObject.FindGameObjectWithTag("Player").transform;
-
+        shrooms = GameObject.FindGameObjectsWithTag("Shroom");
     }
 
     // Update is called once per frame
     void Update()
     {
-        float distance = Vector3.Distance(transform.position,
-            player.position);
+        GameObject eatenShroom = null;
+
+        foreach (var shroom in shrooms)
+        {
+            if (
+                Vector3.Distance(transform.position, shroom.transform.position) <= 
+                nearRadius + effectRadius
+                )
+            {
+                eatenShroom = shroom;
+            }
+        }
+
+        if (!eatenShroom)
+        {
+            return;
+        }
         
+        float distance = Vector3.Distance(transform.position,
+            eatenShroom.transform.position);
         
         if (distance <= nearRadius) // play the peaceful music and remove shaders
         {
-            musicController.OnEnterMushroomNear(mushroomArea);
-
-
-            effectStackController.MushroomArea = MushroomAreas.None;
+            var mushroomArea = eatenShroom.GetComponent<Shroom>()?.Mushroom ??
+                               MushroomAreas.Mushroom2;
             
+            musicController.OnEnterMushroomNear(mushroomArea);
+            effectStackController.MushroomArea = MushroomAreas.None;
         }
         else if (distance >= nearRadius && distance <= effectRadius) // play the high intensity music and shaders increasing in intensity as you get closer 
         {
+            var mushroomArea = eatenShroom.GetComponent<Shroom>()?.Mushroom ??
+                               MushroomAreas.Mushroom2;
             musicController.OnEnterMushroomArea(mushroomArea);
 
             float effectPercentage = 1 - (distance - nearRadius) / (effectRadius - nearRadius);
---
+++ Assets/Scripts/Shroom.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shroom : MonoBehaviour
+{
+    public MushroomAreas Mushroom;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
